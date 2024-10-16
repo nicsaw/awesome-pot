a479aeff5ff7ab239c79f6ec15e1c176a3ec2d65
SAW <99191294+nicsaw@users.noreply.github.com>
2024-10-17 04:26:01 +1100

    Implement EDIT and DELETE items

--- honeypot-web/app.py
+++ honeypot-web/app.py
@@ -4,8 +4,8 @@ from user_agents import parse
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="templates")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sap.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sap.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
@@ -18,7 +18,32 @@ class User(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"<User {self.username}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "password": self.password
+        }
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    website = db.Column(db.String(999), nullable=False)
+    username = db.Column(db.String(999), nullable=False)
+    password = db.Column(db.String(999), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.datetime.now)
+
+    def to_dict(self):
+        return {
+            "userID": self.userID,
+            "id": self.id,
+            "website": self.website,
+            "username": self.username,
+            "password": self.password
+        }
 
 def log_event(request: Request, **kwargs):
     log_entry = {
@@ -28,7 +53,7 @@ def log_event(request: Request, **kwargs):
         "request_method": request.method,
         "request_path": request.path,
         "request_headers": dict(request.headers),
-        "user_agent": str(parse(request.headers.get('User-Agent')))
+        "user_agent": str(parse(request.headers.get("User-Agent")))
     }
 
     extra_fields = {k: v for k, v in kwargs.items() if k not in log_entry}
@@ -36,20 +61,63 @@ def log_event(request: Request, **kwargs):
 
     logging.info(json.dumps(log_entry))
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    log_event(request, event_type="access_index")
-    return render_template("index.html")
+    log_event(request, event_type=f"access_index_{request.method.upper()}")
 
-@app.route("/register", methods=['GET', 'POST'])
+    if request.method == "POST":
+        website = request.form["website"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        newItem = Item(
+            userID=1, # TODO: ========== DONT FORGET TO CHANGE THIS ==========
+            website=website,
+            username=username,
+            password=password
+        )
+
+        db.session.add(newItem)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+    return render_template("index.html", items=Item.query.all())
+
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    targetItem = Item.query.get_or_404(id)
+    if request.method == "POST":
+        try:
+            targetItem.website = request.form["website"]
+            targetItem.username = request.form["username"]
+            targetItem.password = request.form["password"]
+            db.session.commit()
+            return redirect(url_for("index"))
+        except Exception as e:
+            print(f"ERROR edit({id}): {e}")
+
+    return render_template("edit.html", item=targetItem)
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    targetItem = Item.query.get_or_404(id)
+    try:
+        db.session.delete(targetItem)
+        db.session.commit()
+    except Exception as e:
+        print(f"ERROR delete({id}): {e}")
+
+    return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     log_event(request, event_type="access_register")
 
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
 
         log_event(
             request,
@@ -61,23 +129,23 @@ def register():
         )
 
         if password != confirm_password:
-            return redirect(url_for('register'))
+            return redirect(url_for("register"))
 
         newUser = User(username=username, email=email, password=password)
         db.session.add(newUser)
         db.session.commit()
 
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
     return render_template("register.html")
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     log_event(request, event_type="access_login")
 
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
 
         log_event(
             request,
@@ -88,9 +156,9 @@ def login():
 
         user = User.query.filter_by(email=email, password=password).first()
         if user:
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
         else:
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
 
     return render_template("login.html")
 
