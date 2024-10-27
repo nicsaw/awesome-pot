5564c0ccbc59076eb5621e00a3af8ac2db489dfb
SAW <99191294+nicsaw@users.noreply.github.com>
2024-10-28 01:03:46 +1100

    simplified log-analyser.py

--- log-analyser.py
+++ log-analyser.py
@@ -2,62 +2,76 @@ import json, requests
 import pandas as pd
 from typing import List, Dict, Set, Any
 
-def json_to_list(file_path: str) -> List[Dict[str, Any]]:
-    log_entries = []
-    try:
-        with open(file_path, 'r') as log_file:
-            for line in log_file:
-                log_entry = json.loads(line)
-                log_entries.append(log_entry)
-    except Exception as e:
-        print(e)
-    return log_entries
+class Analyser:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.logs: List[Dict[str, Any]] = self.json_to_list(file_path)
+        # self.add_ip_info(self.logs)
+        self.df: pd.DataFrame = pd.DataFrame(self.logs)
 
-def get_all_keys(log_entries) -> Set[str]:
-    return {key for entry in log_entries for key in entry.keys()}
+    def json_to_list(self, file_path: str) -> List[Dict[str, Any]]:
+        log_entries = []
+        try:
+            with open(file_path, 'r') as log_file:
+                for line in log_file:
+                    log_entry = json.loads(line)
+                    log_entries.append(log_entry)
+        except Exception as e:
+            print(e)
+        return log_entries
 
-def filter(log_entries, filter_key=None, filter_value=None) -> List[Dict[str, Any]]:
-    if filter_key and filter_value == None:
-        return [entry for entry in log_entries if filter_key in entry]
-    return [entry for entry in log_entries if entry.get(filter_key) == filter_value]
+    def get_all_keys(self, log_entries) -> Set[str]:
+        return {key for entry in log_entries for key in entry.keys()}
 
-def get_values(log_entries, key):
-    return [entry[key] for entry in log_entries if key in entry]
+    def filter(self, log_entries=None, filter_key=None, filter_value=None) -> List[Dict[str, Any]]:
+        log_entries = log_entries or self.logs
+        if filter_key and filter_value == None:
+            return [entry for entry in log_entries if filter_key in entry]
+        return [entry for entry in log_entries if entry.get(filter_key) == filter_value]
 
-def get_ip_info(ip: str):
-    try:
-        response = requests.get(f"https://ipinfo.io/{ip}/json", timeout=5)
-        data = response.json()
-        data.pop("ip", None)
-        data.pop("readme", None)
+    def get_values(self, key, log_entries=None) -> List[Any]:
+        log_entries = log_entries or self.logs
+        return [entry[key] for entry in log_entries if key in entry]
 
-        return data
-    except Exception as e:
-        print(f"ERROR get_ip_info({ip}): {e}")
+    def get_unique_values(self, key: str, log_entries=None) -> Set[Any]:
+        log_entries = log_entries or self.logs
+        return set(self.get_values(key, log_entries))
 
-def add_ip_info(logs):
-    unique_ips = {item["src_ip"] for item in logs}
-    ip_info_mapping = {ip: get_ip_info(ip) for ip in unique_ips}
+    def get_ip_info(self, ip: str):
+        try:
+            response = requests.get(f"https://ipinfo.io/{ip}/json", timeout=5)
+            data = response.json()
+            data.pop("ip", None)
+            data.pop("readme", None)
 
-    for entry in logs:
-        ip_info = ip_info_mapping.get(entry["src_ip"], {})
-        entry.update(ip_info)
+            return data
+        except Exception as e:
+            print(f"ERROR get_ip_info({ip}): {e}")
+
+    def add_ip_info(self, log_entries=None):
+        log_entries = log_entries or self.logs
+        unique_ips = self.get_unique_values("src_ip", log_entries=None)
+        ip_info_mapping = {ip: self.get_ip_info(ip) for ip in unique_ips}
+
+        for entry in log_entries:
+            ip_info = ip_info_mapping.get(entry["src_ip"], {})
+            entry.update(ip_info)
+
+    def export_frequency_counts(self, columns: List[str]):
+        for col in columns:
+            filename = f"cowrie_freq_{col}.csv"
+            self.df[col].value_counts().sort_values(ascending=False).to_csv(filename)
+            print(f"Created {filename}")
+
+    def export_key_value(self, filter_key=None, filter_value=None) -> List[Dict[str, Any]]:
+        self.df[self.df[filter_key] == filter_value].to_csv(f"cowrie_{filter_key}-{filter_value}.csv")
 
 FILE_PATH = "/Users/nicholassaw/Downloads/cowrie/var/log/cowrie/cowrie.json"
-logs: List[Dict[str, Any]] = json_to_list(FILE_PATH)
-df = pd.read_json(FILE_PATH, lines=True)
+analyser = Analyser(FILE_PATH)
+
+analyser.export_frequency_counts(["username", "password", "src_ip", "input"])
 
 # Number of distinct IPs
-print(f"{len(set(get_values(logs, "src_ip")))}")
+print(f"Number of distinct IPs: {len(set(analyser.get_values("src_ip")))}")
 
-# Most common sernames
-print(df["username"].value_counts().sort_values(ascending=False))
-
-# Most common passwords
-print(df["password"].value_counts().sort_values(ascending=False))
-
-add_ip_info(logs)
-
-df = pd.DataFrame(logs)
-
-df.to_csv("cowrie_logs.csv", index=False)
+analyser.export_key_value("src_ip", "134.122.33.75")
